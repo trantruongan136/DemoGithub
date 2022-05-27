381941158d138bc4d78e7b14da53f196242194b7
Tran Truong An <trantruongan1236@gmail.com>
2022-05-27 21:48:12 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,24 +12,22 @@ class HelloApp extends StatefulWidget {
 }
 
 class _HelloAppState extends State<HelloApp> {
+  int _convat = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       home: Scaffold(
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: Container(
-          height: 50,
-          color: Colors.amber,
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-          hoverColor: Colors.red,
-          backgroundColor: Colors.green,
-          // clipBehavior: Clip.hardEdge,
-          // focusElevation: 20,
-        ),
+        // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+
+        //   },
+        //   child: const Icon(Icons.add),
+        //   hoverColor: Colors.red,
+        //   backgroundColor: Colors.green,
+        //   // clipBehavior: Clip.hardEdge,
+        //   // focusElevation: 20,
+        // ),
         appBar: AppBar(
           title: const Text('Github App'),
           actions: [
@@ -40,74 +38,40 @@ class _HelloAppState extends State<HelloApp> {
         ),
         body: Column(
           children: [
-            Wrap(
-              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber,
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  height: 100,
-                  width: 100,
-                  color: Colors.blue,
-                ),
-                TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'Hello',
-                      style: TextStyle(fontSize: 50),
-                    ),
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.green[100],
-                      padding: const EdgeInsets.all(20),
-                )),
-                Container(
-                  margin: EdgeInsets.all(10),
-                  child: ElevatedButton(onPressed: (){}, 
-                  child: const Icon(Icons.call),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red,
-                    padding: const EdgeInsets.all(20),
-
-                  ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(10),
-                  child: OutlinedButton(onPressed: (){}, 
-                  child: const Icon(Icons.call),
-                  style: ElevatedButton.styleFrom(
-                    // primary: Colors.red,
-                    padding: const EdgeInsets.all(20),
-
-                  ),
-                  ),
-                )
-              ],
+            ListTile(
+              title: const Text('Con voi'),
+              leading:
+                  Radio(value: 0, groupValue: _convat, onChanged: chonDongVat),
+  //            hoverColor: Colors.red,
             ),
-            Container(
-              child: const RadioListTile(value: 1, groupValue: 1, onChanged: null,
-                title: Text('Male'),
-              ),
+            ListTile(
+              title: const Text('Con heo'),
+              leading:
+                  Radio(value: 1, groupValue: _convat, onChanged: chonDongVat),
             ),
-            Container(
-              child: const RadioListTile(value: 0, groupValue: 1, onChanged: null,
-                title: Text('Female'),
-              ),
-            )
+            RadioListTile(
+                value: 2, groupValue: _convat, onChanged: chonDongVat,
+                title: const Text('Con su tu'),
+                selected: true,
+                selectedTileColor: Colors.amber,
+                subtitle: const Text('Chua te son lam'),
+                ),
+            ElevatedButton(onPressed: (){
+              ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Hello'), 
+              duration: Duration(seconds: 5),)
+            );
+            }, child: Icon(Icons.shower))
           ],
         ),
       ),
     );
+  }
+
+  void chonDongVat(value) {
+    setState(() {
+      _convat = int.parse(value.toString());
+      
+    });
   }
 }
